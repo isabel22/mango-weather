5ddb80b210f4f0abbed7c845f916c16bf3e2717b
Isabel Garcia <isabelgg22@gmail.com>
2018-05-02 12:40:44 +0200

    adds specs for weather_by_city_name

--- Gemfile
+++ Gemfile
@@ -39,6 +39,7 @@ group :development, :test do
   # Adds support for Capybara system testing and selenium driver
   gem 'capybara', '~> 2.13'
   gem 'rspec-rails', '~> 3.7'
+  gem 'webmock'
 end
 
 group :development do
---
+++ spec/services/climate_spec.rb
@@ -0,0 +1,29 @@
+require 'spec_helper'
+
+describe MangoWeather::Climate do
+  describe 'weather_by_city_name' do
+    it 'gets data of current weather for a specific city' do
+      filepath = File.join(Rails.root + "spec/support/fixtures/weather_by_city_name.json")
+      climate_data = File.open(filepath).read
+      parsed_climate = JSON.parse(climate_data, :symbolize_names => true)
+
+      app_id = SECRETS["defaults"]["open_weather_map"]
+      stub_request(:get, "http://api.openweathermap.org/data/2.5/weather?APPID=#{app_id}&q=,Republic%20of%20Guatemala,%20GT&units=metric").
+         with(
+           headers: {
+          'Accept'=>'*/*',
+          'Accept-Encoding'=>'gzip;q=1.0,deflate;q=0.6,identity;q=0.3',
+          'Host'=>'api.openweathermap.org',
+          'User-Agent'=>'Ruby'
+           }).
+         to_return(status: 200, body: climate_data, headers: {})
+
+      climate = MangoWeather::Climate.weather_by_city_name("Republic of Guatemala, GT")
+
+      expect(climate["name"]).to eq(parsed_climate[:name])
+      expect(climate["weather"].first["description"]).to eq(parsed_climate[:weather].first[:description])
+      expect(climate["main"]["temp"]).to eq(parsed_climate[:main][:temp])
+      expect(climate["clouds"]["all"]).to eq(parsed_climate[:clouds][:all])
+    end
+  end
+end
