157b11725e2030b05ba3685b165a8711a68d2d72
Isabel Garcia <isabelgg22@gmail.com>
2018-05-02 12:51:25 +0200

    adds spec for weather_by_city_id

--- spec/services/climate_spec.rb
+++ spec/services/climate_spec.rb
@@ -26,4 +26,30 @@ describe MangoWeather::Climate do
       expect(climate["clouds"]["all"]).to eq(parsed_climate[:clouds][:all])
     end
   end
+
+  describe 'weather_by_city_id' do
+    it 'gets data of current weather for a specific city' do
+      filepath = File.join(Rails.root + "spec/support/fixtures/weather_by_city_id.json")
+      climate_data = File.open(filepath).read
+      parsed_climate = JSON.parse(climate_data, :symbolize_names => true)
+
+      app_id = SECRETS["defaults"]["open_weather_map"]
+      stub_request(:get, "http://api.openweathermap.org/data/2.5/weather?APPID=#{app_id}&id=3595528&units=metric").
+         with(
+           headers: {
+          'Accept'=>'*/*',
+          'Accept-Encoding'=>'gzip;q=1.0,deflate;q=0.6,identity;q=0.3',
+          'Host'=>'api.openweathermap.org',
+          'User-Agent'=>'Ruby'
+           }).
+         to_return(status: 200, body: climate_data, headers: {})
+
+      climate = MangoWeather::Climate.weather_by_city_id(3595528)
+
+      expect(climate["name"]).to eq(parsed_climate[:name])
+      expect(climate["weather"].first["description"]).to eq(parsed_climate[:weather].first[:description])
+      expect(climate["main"]["temp"]).to eq(parsed_climate[:main][:temp])
+      expect(climate["clouds"]["all"]).to eq(parsed_climate[:clouds][:all])
+    end
+  end
 end
