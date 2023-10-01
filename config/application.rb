297efbf051852f67d92812ca1cf917829ae9b587
Isabel Garcia <isabelgg22@gmail.com>
2023-10-01 13:26:26 +0200

    Upgrade Rails to version 6.1

--- config/application.rb
+++ config/application.rb
@@ -9,10 +9,16 @@ Bundler.require(*Rails.groups)
 module MangoWeather
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
-    config.load_defaults 5.1
-    config.autoload_paths += %W(#{config.root}/lib)
+    config.load_defaults 6.1
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+
+    # These settings can be overridden in specific environments using the files
+    # in config/environments, which are processed later.
+    #
+    config.time_zone = "Central Time (US & Canada)"
+    config.eager_load_paths << Rails.root.join("extras")
+    config.eager_load_paths << Rails.root.join("lib")
   end
 end
