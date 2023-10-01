61ce7b1d0fe16059bc6ccb93c67e6ab3103a41a4
Isabel Garcia <isabelgg22@gmail.com>
2023-10-01 15:02:06 +0200

    Upgrade Rails to version 7.1 (#19)

--- db/schema.rb
+++ db/schema.rb
@@ -10,6 +10,5 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_10_01_111912) do
-
+ActiveRecord::Schema[7.1].define(version: 2023_10_01_125658) do
 end
