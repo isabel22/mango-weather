e33e85052e99d08cef282c39561a6427dac113c0
Isabel Garcia <isabelgg22@gmail.com>
2023-10-01 22:31:15 +0200

    fix specs

--- lib/mango_weather/climate.rb
+++ lib/mango_weather/climate.rb
@@ -4,19 +4,19 @@ module MangoWeather
   class Climate
     # List of city ID city.list.json.gz can be downloaded here:
     # http://bulk.openweathermap.org/sample/
-    def self.weather_by_city_name(city_name)
+    def self.weather_by_city_name(city_name:)
       new.weather_by_city_name(city_name)
     end
 
-    def self.weather_by_city_id(city_id)
+    def self.weather_by_city_id(city_id:)
       new.weather_by_city_id(city_id)
     end
 
-    def self.weather_for_cities(cities_array)
+    def self.weather_for_cities(cities_array:)
       new.weather_for_cities(cities_array)
     end
 
-    def self.forecast(latitude, longitude)
+    def self.forecast(latitude:, longitude:)
       new.forecast(latitude, longitude)
     end
 
--- spec/services/climate_spec.rb
+++ spec/services/climate_spec.rb
@@ -1,136 +1,59 @@
 require 'spec_helper'
 
 describe MangoWeather::Climate do
+  let(:client) do
+    instance_double(
+      OpenWeather::Client,
+      current_weather: { 'name': 'Guatemala' },
+      current_city_id: { 'id': 3595528 },
+      current_cities_id: {
+        'list': [{name: 'Guatemala'}, {name: 'Berlin'}]
+      },
+      one_call: {'list': []}
+    )
+  end
+
+  before do
+    allow(OpenWeather::Client).to receive(:new).and_return(client)
+  end
+
   describe 'weather_by_city_name' do
-    it 'gets data of current weather for a specific city' do
-      filepath = File.join(Rails.root + "spec/support/fixtures/weather_by_city_name.json")
-      climate_data = File.read(filepath)
-      parsed_climate = JSON.parse(climate_data, :symbolize_names => true)
+    subject { described_class.weather_by_city_name(city_name: 'Republic of Guatemala, GT') }
 
-      app_id = SECRETS["defaults"]["open_weather_map"]
-      stub_request(:get, "http://api.openweathermap.org/data/2.5/weather?APPID=#{app_id}&q=,Republic%20of%20Guatemala,%20GT&units=metric").
-         with(
-           headers: {
-          'Accept'=>'*/*',
-          'Accept-Encoding'=>'gzip;q=1.0,deflate;q=0.6,identity;q=0.3',
-          'Host'=>'api.openweathermap.org',
-          'User-Agent'=>'Ruby'
-           }).
-         to_return(status: 200, body: climate_data, headers: {})
+    it 'calls the method weather_by_city_name with the parameters' do
+      expect(described_class).to receive(:weather_by_city_name).with(city_name: 'Republic of Guatemala, GT')
 
-      climate = MangoWeather::Climate.weather_by_city_name("Republic of Guatemala, GT")
-
-      expect(climate["name"]).to eq(parsed_climate[:name])
-      expect(climate["weather"].first["description"]).to eq(parsed_climate[:weather].first[:description])
-      expect(climate["main"]["temp"]).to eq(parsed_climate[:main][:temp])
-      expect(climate["clouds"]["all"]).to eq(parsed_climate[:clouds][:all])
+      subject
     end
   end
 
   describe 'weather_by_city_id' do
-    it 'gets data of current weather for a specific city' do
-      filepath = File.join(Rails.root + "spec/support/fixtures/weather_by_city_id.json")
-      climate_data = File.read(filepath)
-      parsed_climate = JSON.parse(climate_data, :symbolize_names => true)
+    subject { described_class.weather_by_city_id(city_id: 3595528) }
 
-      app_id = SECRETS["defaults"]["open_weather_map"]
-      stub_request(:get, "http://api.openweathermap.org/data/2.5/weather?APPID=#{app_id}&id=3595528&units=metric").
-         with(
-           headers: {
-          'Accept'=>'*/*',
-          'Accept-Encoding'=>'gzip;q=1.0,deflate;q=0.6,identity;q=0.3',
-          'Host'=>'api.openweathermap.org',
-          'User-Agent'=>'Ruby'
-           }).
-         to_return(status: 200, body: climate_data, headers: {})
+    it 'calls the method weather_by_city_id with the specific id' do
+      expect(described_class).to receive(:weather_by_city_id).with(city_id: 3595528)
 
-      climate = MangoWeather::Climate.weather_by_city_id(3595528)
-
-      expect(climate["name"]).to eq(parsed_climate[:name])
-      expect(climate["weather"].first["description"]).to eq(parsed_climate[:weather].first[:description])
-      expect(climate["main"]["temp"]).to eq(parsed_climate[:main][:temp])
-      expect(climate["clouds"]["all"]).to eq(parsed_climate[:clouds][:all])
+      subject
     end
   end
 
   describe 'weather_for_cities' do
-    it 'gets data of current weather for multiple cities' do
-      filepath = File.join(Rails.root + "spec/support/fixtures/weather_for_cities.json")
-      climate_data = File.read(filepath)
-      parsed_climate = JSON.parse(climate_data, :symbolize_names => true)
+    subject { described_class.weather_for_cities(cities_array: [3595528,2950159]) }
 
-      app_id = SECRETS["defaults"]["open_weather_map"]
-      stub_request(:get, "http://api.openweathermap.org/data/2.5/group?APPID=#{app_id}&id=3595528,2950159&units=metric").
-         with(
-           headers: {
-          'Accept'=>'*/*',
-          'Accept-Encoding'=>'gzip;q=1.0,deflate;q=0.6,identity;q=0.3',
-          'Host'=>'api.openweathermap.org',
-          'User-Agent'=>'Ruby'
-           }).
-         to_return(status: 200, body: climate_data, headers: {})
-
-      climate = MangoWeather::Climate.weather_for_cities([3595528, 2950159])
-
-      expect(climate["cnt"]).to eq(parsed_climate[:cnt])
-      expect(climate["list"].count).to eq(parsed_climate[:list].count)
-      expect(climate["list"].first["weather"].first["description"]).to eq(
-        parsed_climate[:list].first[:weather].first[:description]
-      )
+    it 'calls the method weather_for_cities with the specific ids' do
+      expect(described_class).to receive(:weather_for_cities).with(cities_array: [3595528,2950159])
+      
+      subject
     end
   end
 
-  describe 'forecast_by_city_name' do
-    it 'gets the weather forecast for a specific city' do
-      filepath = File.join(Rails.root + "spec/support/fixtures/forecast_by_city_name.json")
-      climate_data = File.read(filepath)
-      parsed_climate = JSON.parse(climate_data, :symbolize_names => true)
+  describe 'forecast' do
+    subject { described_class.forecast(longitude: -90.25, latitude: 15.5) }
 
-      app_id = SECRETS["defaults"]["open_weather_map"]
-      stub_request(:get, "http://api.openweathermap.org/data/2.5/forecast?APPID=#{app_id}&cnt=3&q=,Republic%20of%20Guatemala,%20GT&units=metric").
-         with(
-           headers: {
-          'Accept'=>'*/*',
-          'Accept-Encoding'=>'gzip;q=1.0,deflate;q=0.6,identity;q=0.3',
-          'Host'=>'api.openweathermap.org',
-          'User-Agent'=>'Ruby'
-           }).
-         to_return(status: 200, body: climate_data, headers: {})
+    it 'calls the method forecast with the specific latitude and longitude' do
+      expect(described_class).to receive(:forecast).with(longitude: -90.25, latitude: 15.5)
 
-      climate = MangoWeather::Climate.forecast_by_city_name("Republic of Guatemala, GT")
-
-      expect(climate["cnt"]).to eq(parsed_climate[:cnt])
-      expect(climate["list"].count).to eq(parsed_climate[:list].count)
-      expect(climate["list"].first["weather"].first["description"]).to eq(
-        parsed_climate[:list].first[:weather].first[:description]
-      )
-    end
-  end
-
-  describe 'forecast_by_city_id' do
-    it 'gets the weather forecast for a specific city_id' do
-      filepath = File.join(Rails.root + "spec/support/fixtures/forecast_by_city_id.json")
-      climate_data = File.read(filepath)
-      parsed_climate = JSON.parse(climate_data, :symbolize_names => true)
-
-      app_id = SECRETS["defaults"]["open_weather_map"]
-      stub_request(:get, "http://api.openweathermap.org/data/2.5/forecast?APPID=#{app_id}&cnt=3&id=3595528&units=metric").
-         with(
-           headers: {
-          'Accept'=>'*/*',
-          'Accept-Encoding'=>'gzip;q=1.0,deflate;q=0.6,identity;q=0.3',
-          'Host'=>'api.openweathermap.org',
-          'User-Agent'=>'Ruby'
-           }).
-         to_return(status: 200, body: climate_data, headers: {})
-
-      climate = MangoWeather::Climate.forecast_by_city_id(3595528)
-
-      expect(climate["cnt"]).to eq(parsed_climate[:cnt])
-      expect(climate["list"].count).to eq(parsed_climate[:list].count)
-      expect(climate["list"].first["weather"].first["description"]).to eq(
-        parsed_climate[:list].first[:weather].first[:description]
-      )
+      subject
     end
   end
 end
